26225b1497d63bb4c2cd0e0276891c59bb181184
Yury Betrozov <betrozov91@gmail.com>
2018-07-14 13:10:43 +0300

    feat(core): base

---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+"use strict";
+
+var gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  fileinclude = require('gulp-file-include'),
+  connect = require('gulp-connect'),
+  shorthand = require('gulp-shorthand'),
+  autoprefixer = require('gulp-autoprefixer'),
+  minifyCSS = require('gulp-minify-css');
+
+
+var scss_path = ['scss/*.scss', 'scss/*/*.scss'],
+  inc_file = 'template/*.html',
+  template_file = 'template/include/*.html',
+  img_file = 'images/*',
+  img_file_sprite = 'images/icon/*.*';
+
+
+gulp.task('connect', function () {
+  connect.server({
+    port: 8888,
+    root: '',
+    livereload: true
+  });
+});
+
+
+gulp.task('scss', function () {
+  gulp.src(scss_path)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(shorthand())
+    //.pipe(minifyCSS())
+    .pipe(gulp.dest('css/'))
+    .pipe(connect.reload());
+});
+
+
+gulp.task('include', function () {
+  gulp.src([inc_file])
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(connect.reload())
+    .pipe(gulp.dest('./'))
+
+});
+
+gulp.task('js', function () {
+  gulp.src('js/*.js') // path to your files
+    .pipe(connect.reload())
+});
+
+
+gulp.task('watch', function () {
+  gulp.watch([inc_file, template_file], ['include']);
+  gulp.watch([scss_path], ['scss']);
+  gulp.watch([img_file_sprite], ['sprite']);
+  gulp.watch(['js/*.js', 'js/libs/*.js'], ['js']);
+});
+
+gulp.task('default', ['connect', 'watch']);
+
+
+
