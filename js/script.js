10dec3049010feeeaf5231f6f325ec23e8088737
Yury Betrozov <betrozov91@gmail.com>
2018-07-15 15:22:19 +0300

    fix(core): fix footer and add term and privacy pages

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $('.j-slider').slick({
     nextArrow: '<div class="next-arrow">' +
     '<button class="slick-prev slick-arrow" aria-label="Previous" type="button" style="">Previous</button>' +
@@ -6,5 +6,14 @@ $(document).ready(function(){
     prevArrow: '<div class="prev-arrow">' +
     '<button class="slick-prev slick-arrow" aria-label="Previous" type="button" style="">Previous</button>' +
     '<svg  viewBox="0 0 60 60"><use xlink:href="#icon-slider"></use></svg></div>'
+  });
+
+  $('.fancybox').fancybox({
+    iframe: {
+      css: {
+        'max-width': 600,
+        'max-height': 400
+      }
+    }
   });
 });
